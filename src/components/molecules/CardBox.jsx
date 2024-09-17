00693b7e6bee73e982425f3f12427b02adca73b7
Facumugen <magomugen@gmail.com>
2024-09-17 00:14:05 -0300

    Modífica CardBox y a ImgCardBox, en tamaño y en nombres

--- src/components/molecules/CardBox.jsx
+++ src/components/molecules/CardBox.jsx
@@ -3,9 +3,9 @@ const CardBox = ({value}) => {
   
   return (
       <div>
-          <div className="w-auto transition-all duration-200 ease-in-out scale-100 bg-[#FFFFFF] shadow-xl cursor-pointer md:w-72 rounded-xl sm:shadow-2xl md:shadow-xl xl:shadow-md hover:shadow-2xl hover:scale-105">
+          <div className="transition-all duration-200 ease-in-out scale-100 bg-[#FFFFFF] shadow-xl cursor-pointer sm:w-96 h-100 w-80 h-90 rounded-xl sm:shadow-2xl md:shadow-xl xl:shadow-md hover:shadow-2xl hover:scale-105">
             <div className="flex items-start justify-center w-auto h-auto overflow-hidden ">
-              <img className="mt-1 brightness-120 rounded-t-xl w-72 h-72 sm:w-80 h-72" src= {value.img} alt="box gourmet" />
+              <img className="mt-1 brightness-120 rounded-t-xl sm:w-96 h-100 w-80 h-90" src= {value.img} alt="box gourmet" />
             </div>
             <div className="flex-col w-auto p-10 mt-0 font-sans text-center text-zinc-600">
                 <p>
--- src/components/molecules/ImgCardBox.jsx
+++ src/components/molecules/ImgCardBox.jsx
@@ -1,19 +1,19 @@
-import CardImg20 from '../../graphics/imagenes/productos/box gourmet/Berlín copia.jpg';
-import CardImg21 from '../../graphics/imagenes/productos/box gourmet/Bruselas copia.jpg';
-import CardImg22 from '../../graphics/imagenes/productos/box gourmet/Camboriú copia.jpg';
-import CardImg23 from '../../graphics/imagenes/productos/box gourmet/Lapampa copia.jpg';
-//import CardImg24 from '../../graphics/imagenes/productos/box gourmet/Caja gourmet copia.jpg';
-import CardImg25 from '../../graphics/imagenes/productos/box gourmet/Milan copia.jpg';
-import CardImg26 from '../../graphics/imagenes/productos/box gourmet/París copia.jpg';
-import CardImg27 from '../../graphics/imagenes/productos/box gourmet/caja zurich copia.jpg';
-import CardImg28 from '../../graphics/imagenes/productos/box gourmet/Santafe copia.jpg';
-import CardImg29 from '../../graphics/imagenes/productos/box gourmet/Papádulce copia.jpg';
-import CardImg30 from '../../graphics/imagenes/productos/box gourmet/Papápicadamini1 copia.jpg';
-import CardImg31 from '../../graphics/imagenes/productos/box gourmet/Papávip copia.jpg';
-import CardImg32 from '../../graphics/imagenes/productos/box gourmet/Papácerveceros copia.jpg';
-import CardImg33 from '../../graphics/imagenes/productos/box gourmet/Papácool copia.jpg';
+import CardImg20 from '../../graphics/imagenes/productos/box gourmet/Caja Berlín.jpg';
+import CardImg21 from '../../graphics/imagenes/productos/box gourmet/Caja Bruselas.jpg';
+import CardImg22 from '../../graphics/imagenes/productos/box gourmet/Caja Camboriú.jpg';
+import CardImg23 from '../../graphics/imagenes/productos/box gourmet/Caja La Pampa.jpg';
+//import CardImg24 from '../../graphics/imagenes/productos/box gourmet/Caja gourmet.jpg';
+import CardImg25 from '../../graphics/imagenes/productos/box gourmet/Caja Milan.jpg';
+import CardImg26 from '../../graphics/imagenes/productos/box gourmet/Caja París.jpg';
+import CardImg27 from '../../graphics/imagenes/productos/box gourmet/Caja Zurich.jpg';
+import CardImg28 from '../../graphics/imagenes/productos/box gourmet/Caja Santa Fe.jpg';
+import CardImg29 from '../../graphics/imagenes/productos/box gourmet/Caja Papá picada mini 1.jpg';
+import CardImg30 from '../../graphics/imagenes/productos/box gourmet/Caja Papá vip.jpg';
+import CardImg31 from '../../graphics/imagenes/productos/box gourmet/Caja Papá cool.jpg';
+import CardImg32 from '../../graphics/imagenes/productos/box gourmet/Caja Papá dulce.jpg';
+import CardImg33 from '../../graphics/imagenes/productos/box gourmet/Caja Papá cerveceros.jpg';
 import CardImg34 from '../../graphics/imagenes/productos/box gourmet/Caja 5j.jpg';
-import CardImg35 from '../../graphics/imagenes/productos/box gourmet/Caja Torgelon o Los Calvos copia.jpg';
+import CardImg35 from '../../graphics/imagenes/productos/box gourmet/Caja Torgelon o Los Calvos.jpg';
 import CardImg36 from '../../graphics/imagenes/productos/box gourmet/Caja desayuno.jpg';
 
 
